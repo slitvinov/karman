9f6fb5e784d95751417e4ef9c88eb6218a10dbaf
Sergey Litvinov <slitvinov@gmail.com>
2023-02-27 11:11:30 +0100

    up

--- 3.c
+++ 3.c
@@ -1,21 +1,23 @@
 #include "grid/octree.h"
 #include "embed.h"
 #include "navier-stokes/centered.h"
+#include "view.h"
 static const double d = 0.0625;
 static const int code[] = {1, 3, 5, 7};
 static const double pos[][2] = {{0, 0}, {2.5, 5.0/6}, {5.0, 10.0/6}, {7.5, 15.0/6}};
-static double Reynolds = 1100.;
-static int maxlevel = 12;
+static double Reynolds = 10;
+static int maxlevel = 6;
 static int icase;
 
-u.n[left]  = dirichlet(1.);
-p[left]    = neumann(0.);
-pf[left]   = neumann(0.);
-u.n[right] = neumann(0.);
-p[right]   = dirichlet(0.);
-pf[right]  = dirichlet(0.);
-u.n[embed] = fabs(y) > 0.45 ? neumann(0.) : dirichlet(0.);
-u.t[embed] = fabs(y) > 0.45 ? neumann(0.) : dirichlet(0.);
+u.n[left]  = dirichlet(1);
+p[left]    = neumann(0);
+pf[left]   = neumann(0);
+u.n[right] = neumann(0);
+p[right]   = dirichlet(0);
+pf[right]  = dirichlet(0);
+u.n[embed] = fabs(y) > 0.45 ? neumann(0) : dirichlet(0);
+u.t[embed] = fabs(y) > 0.45 ? neumann(0) : dirichlet(0);
+u.r[embed] = fabs(y) > 0.45 ? neumann(0) : dirichlet(0);
 face vector muv[];
 
 int main(int argc, char **argv) {
@@ -24,9 +26,9 @@ int main(int argc, char **argv) {
     exit(1);
   }
   icase = atoi(argv[1]);
-  L0 = 4;
-  origin (-0.5, -L0/2.);
-  N = 1024;
+  L0 = 1;
+  origin (-0.5, -L0/2., -0.5);
+  N = 64;
   mu = muv;
   run();
 }
@@ -44,6 +46,8 @@ event init (t = 0)
     double p0;
     p0 = 0.5 - y;
     p0 = min(p0, 0.5 + y);
+    p0 = min(p0, 0.125 - z);
+    p0 = min(p0, 0.125 + z);
     for (i = 0; i < icase; i++) {
       p0 = min(p0, sq(x - pos[i][0] * d) + sq(y - pos[i][1] * d) - sq(d/2));
       p0 = min(p0, sq(x - pos[i][0] * d) + sq(y + pos[i][1] * d) - sq(d/2));
@@ -57,25 +61,18 @@ event logfile (i += 10) {
   fprintf (stderr, "%d %g %d %d\n", i, t, mgp.i, mgu.i);
 }
 
-event movies (i += 50; t <= 100)
+event movies (i += 1; t <= 100)
 {
   static long iframe = 0;
   char path[FILENAME_MAX];
   scalar omega[], m[];
   vorticity (u, omega);
   foreach()
-    m[] = cs[] - 0.5;
-  sprintf(path, "%d.0.%09ld.png", code[icase - 1], iframe);
-  output_ppm (omega, file = path, box = {{-d, -d}, {d, d}},
-	      min = -200, max = 200, linear = false, mask = m);
-  sprintf(path, "%d.1.%09ld.png", code[icase - 1], iframe);
-  output_ppm (omega, file = path, box = {{-d, -3.5 * d}, {10 * d, 3.5 * d}},
-	      min = -200, max = 200, linear = false, mask = m);
+    omega[] = ((u.y[1] - u.y[-1]) - (u.x[0,1] - u.x[0,-1]))/(4.*Delta);
+  view (fov = 15.65, quat = {-0.52,0.31,0.38,-0.7},
+	tx = -0.045, ty = 0.015, width = 640, height = 480, bg = {0,0,0});  
+  isosurface ("u.x", 0.5, color = "level");
   sprintf(path, "%d.2.%09ld.png", code[icase - 1], iframe);
-  output_ppm (omega, file = path, box = {{-0.5, -0.45}, {3.5, 0.45}},
-	      min = -200, max = 200, linear = false, mask = m);
+  save(path);
   iframe++;
-}
-event adapt (i++) {
-  adapt_wavelet ({cs,u}, (double[]){1e-2, 3e-3, 3e-3}, maxlevel, 4);
 }
--- Makefile
+++ Makefile
@@ -2,11 +2,17 @@
 .SUFFIX:
 .SUFFIX: .c
 
+BASILISK = $(HOME)/basilisk/src
 MPICC = mpicc
 MPICCFLAGS = -O2 -g
 QCC = qcc
-all: _2 _3
+all: 2 3 sphere
 _2.c: 2.c; $(QCC) $(QCCFLAGS) -D_MPI=1 2.c -source
 _3.c: 3.c; $(QCC) $(QCCFLAGS) -D_MPI=1 3.c -source
-.c:; $(MPICC) -o $@ $(MPICCFLAGS) $< -lm
-clean:; rm -f _2 _2.c _3 _3.c
+_sphere.c: sphere.c; $(QCC) $(QCCFLAGS) -D_MPI=1 sphere.c -source
+
+2: _2.c; $(MPICC) -o $@ $(MPICCFLAGS) _2.c -lm
+3: _3.c; $(MPICC) -o $@ $(MPICCFLAGS) _3.c $(BASILISK)/gl/libglutils.a $(BASILISK)/gl/libfb_osmesa.a  `pkg-config --libs osmesa glu` -lm
+sphere: _sphere.c; $(MPICC) -o sphere $(MPICCFLAGS) _sphere.c $(BASILISK)/gl/libglutils.a $(BASILISK)/gl/libfb_osmesa.a  `pkg-config --libs osmesa glu` -lm
+
+clean:; rm -f _2.c _3.c 2 3
