9d291a27fde750b192ce66b29750143075efc6b4
Sergey Litvinov <slitvinov@gmail.com>
2023-02-27 12:45:57 +0100

    up

--- 3.c
+++ 3.c
@@ -18,6 +18,7 @@ pf[right]  = dirichlet(0);
 u.n[embed] = fabs(y) > 0.45 ? neumann(0) : dirichlet(0);
 u.t[embed] = fabs(y) > 0.45 ? neumann(0) : dirichlet(0);
 u.r[embed] = fabs(y) > 0.45 ? neumann(0) : dirichlet(0);
+
 face vector muv[];
 
 int main(int argc, char **argv) {
@@ -28,7 +29,7 @@ int main(int argc, char **argv) {
   icase = atoi(argv[1]);
   L0 = 1;
   origin (-0.5, -L0/2., -0.5);
-  N = 64;
+  N = 128;
   mu = muv;
   run();
 }
@@ -44,17 +45,19 @@ event init (t = 0)
   foreach_vertex() {
     int i;
     double p0;
+    p0 = HUGE;
     p0 = 0.5 - y;
     p0 = min(p0, 0.5 + y);
-    p0 = min(p0, 0.125 - z);
-    p0 = min(p0, 0.125 + z);
     for (i = 0; i < icase; i++) {
       p0 = min(p0, sq(x - pos[i][0] * d) + sq(y - pos[i][1] * d) - sq(d/2));
       p0 = min(p0, sq(x - pos[i][0] * d) + sq(y + pos[i][1] * d) - sq(d/2));
     }
     phi[] = p0;
   }
+  view (fov = 5, theta = 0.05, relative = false, width = 1024, height = 1024);
   fractions(phi, cs, fs);
+  draw_vof ("cs", "fs");
+  save("cs.png");
 }
 
 event logfile (i += 10) {
@@ -69,9 +72,8 @@ event movies (i += 1; t <= 100)
   vorticity (u, omega);
   foreach()
     omega[] = ((u.y[1] - u.y[-1]) - (u.x[0,1] - u.x[0,-1]))/(4.*Delta);
-  view (fov = 15.65, quat = {-0.52,0.31,0.38,-0.7},
-	tx = -0.045, ty = 0.015, width = 640, height = 480, bg = {0,0,0});  
-  isosurface ("u.x", 0.5, color = "level");
+  view (fov = 5, theta = 0.05, relative = false);
+  isosurface ("omega", 1, color = "level");
   sprintf(path, "%d.2.%09ld.png", code[icase - 1], iframe);
   save(path);
   iframe++;
