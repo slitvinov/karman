a26a4777a9a0ac9147803340cc72433ea9e97ebb
Sergey Litvinov <slitvinov@gmail.com>
2023-11-12 15:47:16 -0500

    up

---
+++ 3d/stl/cylinder.py
@@ -0,0 +1,45 @@
+import struct
+import math
+
+L = 4
+R = 1
+n = 10
+
+ver = []
+tri = []
+
+def circle(z, orient):
+    i0 = len(ver)
+    ver.append((0, 0, z))
+    for i in range(n):
+        p = 2 * math.pi * i / n
+        x = R * math.cos(p)
+        y = R * math.sin(p)
+        ver.append((x, y, z))
+    for i in range(n):
+        j = i + 1
+        if j == n: j = 0
+        a, b, c = i0, i + 1 + i0, j + 1 + i0
+        if orient:
+            tri.append((b, a, c))
+        else:
+            tri.append((a, b, c))
+circle(-L/2, True)
+circle(L/2, False)
+for i in range(n):
+    a = i + 1
+    b = 1 if i == n - 1 else i + 2
+    c = a + (n + 1)
+    d = b + (n + 1)
+    tri.append((a, b, c))
+    tri.append((c, b, d))
+
+with open("a.stl", "wb") as out:
+    out.write(bytes(80 + 4))
+    nt = 0
+    for i, j, k in tri:
+        out.write(struct.pack('12f', 0, 0, 0, *ver[i], *ver[j], *ver[k]))
+        out.write(bytes(2))
+        nt += 1
+    out.seek(80)
+    out.write(struct.pack('<I', nt))
