65740952a7b984c02e3ae45833d45c72bfdf698f
Sergey Litvinov <slitvinov@gmail.com>
2023-12-15 07:03:06 +0100

    r = 2000

--- 3d/deploy/run.sh
+++ 3d/deploy/run.sh
@@ -7,7 +7,7 @@ module load daint-mc
 module load cray-mpich
 
 make MPICC=cc 'MPICCFLAGS = -O2 -g' && {
-    r=220
+    r=2000
     mkdir -p $r
     srun ./cylinder -v -r $r -l 8 -m 11 -p 100 -e 2600 -f $r/force.dat -o $r/h
 }
