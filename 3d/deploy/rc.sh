d382d8c91bbc326fb4cc420f1899a3080997daa8
Sergey Litvinov <slitvinov@boslogin01.rc.fas.harvard.edu>
2023-12-15 12:40:47 -0500

    update rc.sh

--- 3d/deploy/rc.sh
+++ 3d/deploy/rc.sh
@@ -5,7 +5,7 @@ module load intel intelmpi
 set -x
 r=$1
 d=$SCRATCH/koumoutsakos_lab/slitvinov/$r
-make 'MPICCFLAGS = -O2 -g' && {
+make MPICC=mpiicx 'MPICCFLAGS = -O2 -g -Wno-deprecated-non-prototype' && {
     mkdir -p $d
     exec srun -n $SLURM_NTASKS --mpi=pmi2 ./cylinder -v -r $r -l 8 -m 12 -p 100 -e 2600 -f $d/force.dat -o $d/h
 }
