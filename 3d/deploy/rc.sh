961cbba1fe61ccaec8a19c3482339a89a33a80d7
Sergey Litvinov <slitvinov@boslogin01.rc.fas.harvard.edu>
2023-12-16 09:54:07 -0500

    rc

--- 3d/deploy/rc.sh
+++ 3d/deploy/rc.sh
@@ -5,5 +5,5 @@ r=$1
 d=$SCRATCH/koumoutsakos_lab/slitvinov/$r
 make MPICC=mpicc 'MPICCFLAGS = -O2 -g' && {
     mkdir -p $d
-    exec srun -n $SLURM_NTASKS --mpi=pmix ./cylinder -v -r $r -l 8 -m 14 -p 100 -e 2600 -f $d/force.dat -o $d/h
+    exec srun -n $SLURM_NTASKS --mpi=pmix ./cylinder -v -r $r -l 8 -m 12 -p 100 -e 2600 -f $d/force.dat -o $d/h
 }
