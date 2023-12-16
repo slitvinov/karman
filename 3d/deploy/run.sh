3bdd3a452bce3c937f39022199b300f6bfc84468
Sergey Litvinov <slitvinov@gmail.com>
2023-12-16 17:12:13 +0100

    up

--- 3d/deploy/run.sh
+++ 3d/deploy/run.sh
@@ -11,5 +11,5 @@ r=$1
 d=$SCRATCH/$r
 make MPICC=mpicc 'MPICCFLAGS = -O2 -g' && {
     mkdir -p $d
-    exec srun -n $SLURM_NTASKS --mpi=pmix ./cylinder -v -r $r -l 8 -m 12 -p 100 -e 2600 -f $d/force.dat -o $d/h
+    exec srun ./cylinder -v -r $r -l 8 -m 12 -p 100 -e 2600 -f $d/force.dat -o $d/h
 }
