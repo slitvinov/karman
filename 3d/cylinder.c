b4d70278c9f9fc644fa40df583e5eb7a49db7f4b
Sergey Litvinov <slitvinov@gmail.com>
2023-12-28 08:26:31 -0500

    clang-format

--- 3d/cylinder.c
+++ 3d/cylinder.c
@@ -27,7 +27,8 @@ static double shape_cylinder(double x, double y, double z) {
 static double shape_sphere(double x, double y, double z) {
   return sq(x) + sq(y) + sq(z) - sq(diameter / 2);
 }
-static const double (*Shape[])(double, double, double) = {shape_cylinder, shape_sphere};
+static const double (*Shape[])(double, double, double) = {shape_cylinder,
+                                                          shape_sphere};
 static const char *shape_names[] = {"cylinder", "sphere"};
 static const double (*shape)(double, double, double);
 
@@ -111,54 +112,54 @@ static double tri_point_distance2(const double a[3], const double b[3],
   return x * x + y * y + z * z;
 }
 
-trace
-double embed_interpolate3(Point point, scalar s, coord p)
-{
+trace double embed_interpolate3(Point point, scalar s, coord p) {
   int i = sign(p.x), j = sign(p.y), k = sign(p.z);
-  if (cs[i,0,0] && cs[0,j,0] && cs[i,j,0] &&
-      cs[0,0,k] && cs[i,0,k] && cs[0,j,k] && cs[i,j,k] ) {
+  if (cs[i, 0, 0] && cs[0, j, 0] && cs[i, j, 0] && cs[0, 0, k] && cs[i, 0, k] &&
+      cs[0, j, k] && cs[i, j, k]) {
     double val_0, val_k;
-    val_0 = (s[0,0,0]*(1. - fabs(p.x)) + s[i,0,0]*fabs(p.x))*(1. - fabs(p.y)) +
-      (s[0,j,0]*(1. - fabs(p.x)) + s[i,j,0]*fabs(p.x))*fabs(p.y);
-    val_k = (s[0,0,k]*(1. - fabs(p.x)) + s[i,0,k]*fabs(p.x))*(1. - fabs(p.y)) +
-      (s[0,j,k]*(1. - fabs(p.x)) + s[i,j,k]*fabs(p.x))*fabs(p.y);
-    return (val_0*(1. - fabs(p.z)) + val_k*fabs(p.z));
-  }
-  else {
+    val_0 =
+        (s[0, 0, 0] * (1. - fabs(p.x)) + s[i, 0, 0] * fabs(p.x)) *
+            (1. - fabs(p.y)) +
+        (s[0, j, 0] * (1. - fabs(p.x)) + s[i, j, 0] * fabs(p.x)) * fabs(p.y);
+    val_k =
+        (s[0, 0, k] * (1. - fabs(p.x)) + s[i, 0, k] * fabs(p.x)) *
+            (1. - fabs(p.y)) +
+        (s[0, j, k] * (1. - fabs(p.x)) + s[i, j, k] * fabs(p.x)) * fabs(p.y);
+    return (val_0 * (1. - fabs(p.z)) + val_k * fabs(p.z));
+  } else {
     double val = s[];
     foreach_dimension() {
       int i = sign(p.x);
-      if (cs[i] )
-	val += fabs(p.x)*(s[i] - s[]);
-      else if (cs[-i] )
-	val += fabs(p.x)*(s[] - s[-i]);
+      if (cs[i])
+        val += fabs(p.x) * (s[i] - s[]);
+      else if (cs[-i])
+        val += fabs(p.x) * (s[] - s[-i]);
     }
     return val;
   }
 }
 
-trace
-void embed_force3(scalar p, vector u, face vector mu, coord * Fp, coord * Fmu)
-{
+trace void embed_force3(scalar p, vector u, face vector mu, coord *Fp,
+                        coord *Fmu) {
   coord Fps = {0}, Fmus = {0};
   foreach (reduction(+ : Fps) reduction(+ : Fmus)) {
     if (cs[] > 0. && cs[] < 1.) {
       coord n, b;
       double area = embed_geometry(point, &b, &n);
-      area *= pow (Delta, dimension - 1);
+      area *= pow(Delta, dimension - 1);
       double Fn = area * embed_interpolate3(point, p, b);
-      foreach_dimension()
-	Fps.x += Fn*n.x;
+      foreach_dimension() Fps.x += Fn * n.x;
       if (constant(mu.x) != 0.) {
-	double mua = 0., fa = 0.;
-	foreach_dimension() {
-	  mua += mu.x[] + mu.x[1];
-	  fa  += fs.x[] + fs.x[1];
-	}
-	mua /= fa;
-	coord dudn = embed_gradient (point, u, b, n);
-	foreach_dimension()
-	  Fmus.x -= area*mua*(dudn.x*(sq (n.x) + 1.) + dudn.y*n.x*n.y + dudn.z*n.x*n.z);
+        double mua = 0., fa = 0.;
+        foreach_dimension() {
+          mua += mu.x[] + mu.x[1];
+          fa += fs.x[] + fs.x[1];
+        }
+        mua /= fa;
+        coord dudn = embed_gradient(point, u, b, n);
+        foreach_dimension() Fmus.x -=
+            area * mua *
+            (dudn.x * (sq(n.x) + 1.) + dudn.y * n.x * n.y + dudn.z * n.x * n.z);
       }
     }
   }
@@ -166,7 +167,7 @@ void embed_force3(scalar p, vector u, face vector mu, coord * Fp, coord * Fmu)
   Fp->x = Fps.x;
   Fp->y = Fps.y;
   Fp->z = Fps.z;
-  
+
   Fmu->x = Fmus.x;
   Fmu->y = Fmus.y;
   Fmu->z = Fmus.z;
@@ -248,7 +249,7 @@ int main(int argc, char **argv) {
           "number)\n"
           "  -f <force file>          force file\n"
           "  -s <STL file>            geometry file\n"
-	  "  -S cylinder|sphere       shape\n"
+          "  -S cylinder|sphere       shape\n"
           "  -d <dump file>           restart simulation\n"
           "\n"
           "Example usage:\n"
@@ -359,15 +360,15 @@ int main(int argc, char **argv) {
         fprintf(stderr, "cylinder: error: -S needs an argument\n");
         exit(1);
       }
-      for (i = 0; /**/ ; i++) {
-	if (i == sizeof shape_names / sizeof *shape_names) {
-	  fprintf(stderr, "cylinder: error: unknown shape '%s'\n", *argv);
-	  exit(1);
-	}
-	if (strcmp(shape_names[i], *argv) == 0) {
-	  shape = Shape[i];
-	  break;
-	}
+      for (i = 0; /**/; i++) {
+        if (i == sizeof shape_names / sizeof *shape_names) {
+          fprintf(stderr, "cylinder: error: unknown shape '%s'\n", *argv);
+          exit(1);
+        }
+        if (strcmp(shape_names[i], *argv) == 0) {
+          shape = Shape[i];
+          break;
+        }
       }
       break;
     case 'o':
