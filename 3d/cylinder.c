b73bd8364c67ec9526454aa9486994a99317c18f
Sergey Litvinov <slitvinov@gmail.com>
2024-01-02 12:24:22 -0500

    up

--- 3d/cylinder.c
+++ 3d/cylinder.c
@@ -580,10 +580,9 @@ event init(t = 0) {
     srand(npe() + seed);
     foreach ()
       if (cs[] == 1) {
-        assert(0);
-        u.x[] += 1e-8 * (1 - 2 * rand() / (double)RAND_MAX);
-        u.y[] += 1e-8 * (1 - 2 * rand() / (double)RAND_MAX);
-        u.z[] += 1e-8 * (1 - 2 * rand() / (double)RAND_MAX);
+        u.x[] += 1e-3 * (1 - 2 * rand() / (double)RAND_MAX);
+        u.y[] += 1e-3 * (1 - 2 * rand() / (double)RAND_MAX);
+        u.z[] += 1e-3 * (1 - 2 * rand() / (double)RAND_MAX);
       }
   }
 }
