54e114e7a598807b083fa3d96f7474ecd778b82e
Sergey Litvinov <slitvinov@holylogin04.rc.fas.harvard.edu>
2024-01-16 07:48:05 -0500

    up

--- 3d/deploy/rc.sh
+++ 3d/deploy/rc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 module load gcc openmpi
 set -x
-r=$1
+r=220
 d=$SCRATCH/koumoutsakos_lab/slitvinov/$r
 make MPICC=mpicc 'MPICCFLAGS = -O2 -g' && {
     mkdir -p $d
-    exec srun -n $SLURM_NTASKS --mpi=pmix ./cylinder -v -r $r -l 7 -m 9 -p 10 -e 10 -f $d/force.dat -S cylinder -o $d/h -z 2.5
+    exec srun -n $SLURM_NTASKS --mpi=pmix ./cylinder -v -r $r -l 8 -m 11 -p 10 -e 3000 -f $d/force.dat -z 50 -S cylinder -o $d/h -b t -d $r/h.000001000.dump
 }
