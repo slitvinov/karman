73bae859e3be0343806f90e3d73749d90c67bc6e
Sergey Litvinov <slitvinov@gmail.com>
2024-01-29 11:22:34 -0500

    dump number of leafs per level

--- 3d/paraview/dump_info.c
+++ 3d/paraview/dump_info.c
@@ -25,40 +25,24 @@ struct DumpHeader {
 };
 static struct DumpHeader header;
 static int *index;
+static long *counter;
 static double *values;
 static int m_level;
 static long traverse(int);
 static double X0, Y0, Z0, L0;
+static long nleaf;
 static const double shift[8][3] = {
     {0, 0, 0}, {0, 0, 1}, {0, 1, 1}, {0, 1, 0},
     {1, 0, 0}, {1, 0, 1}, {1, 1, 1}, {1, 1, 0},
 };
-static FILE *xyz_file, *attr_file;
-static long ncell_total;
 
 int main(int argc, char **argv) {
-  FILE *file;
-  long i, j, nvect, nattr;
+  long i;
   unsigned len;
   char *input_path;
   int Verbose;
   double o[4];
-  char xyz_path[FILENAME_MAX], attr_path[FILENAME_MAX], xdmf_path[FILENAME_MAX],
-      *xyz_base, *attr_base;
   char **names;
-  char *path = "a";
-  snprintf(xyz_path, sizeof xyz_path, "%s.xyz.raw", path);
-  snprintf(attr_path, sizeof attr_path, "%s.attr.raw", path);
-  snprintf(xdmf_path, sizeof xdmf_path, "%s.xdmf2", path);
-  xyz_base = xyz_path;
-  attr_base = attr_path;
-  for (j = 0; xyz_path[j] != '\0'; j++) {
-    if (xyz_path[j] == '/' && xyz_path[j + 1] != '\0') {
-      xyz_base = &xyz_path[j + 1];
-      attr_base = &attr_path[j + 1];
-    }
-  }
-
   Verbose = 0;
   while (*++argv != NULL && argv[0][0] == '-')
     switch (argv[0][1]) {
@@ -100,7 +84,7 @@ int main(int argc, char **argv) {
     names[i] = malloc((len + 1) * sizeof *names[i]);
     FREAD(names[i], sizeof *names[i], len);
     names[i][len] = '\0';
-    fprintf(stderr, "name[%d]: %s\n", len, names[i]);
+    fprintf(stderr, "name: %s\n", names[i]);
   }
   FREAD(o, sizeof o, 1);
   fprintf(stderr, "origin: [%g %g %g]\n", o[0], o[1], o[2]);
@@ -111,87 +95,25 @@ int main(int argc, char **argv) {
   fprintf(stderr, "size: %g\n", o[3]);
   m_level = 0;
   index = NULL;
+  counter = NULL;
   values = malloc(header.len * sizeof *values);
-  if ((xyz_file = fopen(xyz_path, "w")) == NULL) {
-    fprintf(stderr, "%s:%d: fail to open '%s'\n", __FILE__, __LINE__, xyz_path);
-    return 1;
-  }
-  if ((attr_file = fopen(attr_path, "w")) == NULL) {
-    fprintf(stderr, "%s:%d: fail to open '%s'\n", __FILE__, __LINE__,
-            attr_path);
-    return 1;
-  }
-  ncell_total = 0;
+  nleaf = 0;
   traverse(0);
-  fprintf(stderr, "ncell_total: %ld\n", ncell_total);
-  if (fclose(xyz_file) != 0) {
-    fprintf(stderr, "dump_info: error: fail to close '%s'\n", xyz_path);
-    exit(1);
-  }
-  if (fclose(attr_file) != 0) {
-    fprintf(stderr, "dump_info: error: fail to close '%s'\n", attr_path);
-    exit(1);
-  }
+  fprintf(stderr, "nleaf: %ld\n", nleaf);
+  for (i = 0; i < m_level; i++)
+    if (counter[i] > 0)
+      fprintf(stderr, "n[%ld]: %ld\n", i, counter[i]);
   free(index);
+  free(counter);
   if (fclose(input_file) != 0) {
     fprintf(stderr, "dump_info: error: fail to close '%s'\n", input_path);
     exit(1);
   }
-  if ((file = fopen(xdmf_path, "w")) == NULL) {
-    fprintf(stderr, "%s:%d: fail to open '%s'\n", __FILE__, __LINE__,
-            xdmf_path);
-    return 1;
-  }
-  fprintf(file,
-          "<Xdmf\n"
-          "    Version=\"2\">\n"
-          "  <Domain>\n"
-          "    <Grid>\n"
-          "      <Topology\n"
-          "          TopologyType=\"Hexahedron\"\n"
-          "          Dimensions=\"%ld\"/>\n"
-          "      <Geometry>\n"
-          "        <DataItem\n"
-          "            Dimensions=\"%ld 3\"\n"
-          "            Format=\"Binary\">\n"
-          "          %s\n"
-          "        </DataItem>\n"
-          "      </Geometry>\n",
-          ncell_total, 8 * ncell_total, xyz_base);
-  j = 0;
-  nvect = 0;
-  nattr = header.len;
-  for (i = 0; i < header.len; i++)
-    fprintf(file,
-            "      <Attribute\n"
-            "          Name=\"%s\"\n"
-            "          Center=\"Cell\">\n"
-            "        <DataItem\n"
-            "            ItemType=\"HyperSlab\"\n"
-            "            Dimensions=\"%ld\"\n"
-            "            Type=\"HyperSlab\">\n"
-            "          <DataItem Dimensions=\"3 1\">\n"
-            "            %ld %ld %ld\n"
-            "          </DataItem>\n"
-            "          <DataItem\n"
-            "              Precision=\"8\"\n"
-            "              Dimensions=\"%ld\"\n"
-            "              Format=\"Binary\">\n"
-            "            %s\n"
-            "          </DataItem>\n"
-            "         </DataItem>\n"
-            "      </Attribute>\n",
-            names[i], ncell_total, j++, nattr + 3 * nvect, ncell_total,
-            (nattr + 3 * nvect) * ncell_total, attr_base);
-  fprintf(file, "    </Grid>\n"
-                "  </Domain>\n"
-                "</Xdmf>\n");
-  fclose(file);
 }
 
 static void process(int level) {
   int i, j;
-  double Delta, l2, x, y, z, epsilon;
+  double Delta, x, y, z;
   float xyz[8 * 3];
   x = 0;
   y = 0;
@@ -202,36 +124,21 @@ static void process(int level) {
     y += Delta * (shift[index[i]][1] - 0.5);
     z += Delta * (shift[index[i]][2] - 0.5);
   }
-  l2 = values[8];
-  epsilon = Delta / 10;
-  if (l2 > -0.60 && l2 <= -0.40) {
-  //if (z <= -epsilon && z + Delta + epsilon >= 0) {
-    j = 0;
-    for (i = 0; i < 8; i++) {
-      xyz[j++] = x + Delta * (shift[i][0] - 0.5);
-      xyz[j++] = y + Delta * (shift[i][1] - 0.5);
-      xyz[j++] = z + Delta * (shift[i][2] - 0.5);
-    }
-    if (fwrite(xyz, sizeof xyz, 1, xyz_file) != 1) {
-      fprintf(stderr, "dump_info: failed to write coordinates\n");
-      exit(1);
-    }
-    if (fwrite(values, sizeof *values, header.len, attr_file) != header.len) {
-      fprintf(stderr, "dump_info: failed to write attributes\n");
-      exit(1);
-    }
-    ncell_total++;
+  j = 0;
+  for (i = 0; i < 8; i++) {
+    xyz[j++] = x + Delta * (shift[i][0] - 0.5);
+    xyz[j++] = y + Delta * (shift[i][1] - 0.5);
+    xyz[j++] = z + Delta * (shift[i][2] - 0.5);
   }
+  nleaf++;
+  counter[level]++;
 }
 
 static long traverse(int level) {
-  enum {
-    leaf = 1 << 1,
-  };
+  enum { leaf = 2 };
   unsigned flags;
   double val;
   long size, size0;
-  int i;
   FREAD(&flags, sizeof flags, 1);
   FREAD(values, sizeof *values, header.len);
   size = values[0];
@@ -244,6 +151,8 @@ static long traverse(int level) {
     while (level + 1 >= m_level) {
       m_level = 2 * m_level + 1;
       index = realloc(index, m_level * sizeof *index);
+      counter = realloc(counter, m_level * sizeof *counter);
+      counter[level] = 0;
     }
     for (index[level + 1] = 0; index[level + 1] < 8; index[level + 1]++)
       size0 += traverse(level + 1);
