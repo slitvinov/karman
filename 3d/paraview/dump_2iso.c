afdd97e827f5aac8f942cd3752089ba4b4e59373
Sergey Litvinov <slitvinov@gmail.com>
2024-01-30 06:55:52 -0500

    up

--- 3d/paraview/dump_2iso.c
+++ 3d/paraview/dump_2iso.c
@@ -23,13 +23,15 @@ struct DumpHeader {
   int i, depth, npe, version;
   struct coord n;
 };
-static struct DumpHeader header;
-static int *index;
-static double *values;
-static int malloc_level;
-static long traverse(int);
-static double X0, Y0, Z0, L0;
-static long nleaf;
+static long traverse(int, void (*)(int, void *), void *);
+static void process(int, void *);
+struct Context {
+  struct DumpHeader header;
+  int *index, malloc_level;
+  double *values, X0, Y0, Z0, L0;
+  long nleaf;
+};
+
 static const double shift[8][3] = {
     {0, 0, 0}, {0, 0, 1}, {0, 1, 1}, {0, 1, 0},
     {1, 0, 0}, {1, 0, 1}, {1, 1, 1}, {1, 1, 0},
@@ -42,16 +44,17 @@ int main(int argc, char **argv) {
   int Verbose;
   double o[4];
   char **names;
+  struct Context context;
   Verbose = 0;
   while (*++argv != NULL && argv[0][0] == '-')
     switch (argv[0][1]) {
     case 'h':
       fprintf(stderr,
-	      "Usage: dump_2iso [-h] [-v] file.dump\n"
-	      "Options:\n"
-	      "  -h                          Print help message and exit\n"
-	      "  -v                          Verbose\n"
-	      "  file.dump                   basilisk dump\n");
+              "Usage: dump_2iso [-h] [-v] file.dump\n"
+              "Options:\n"
+              "  -h                          Print help message and exit\n"
+              "  -v                          Verbose\n"
+              "  file.dump                   basilisk dump\n");
       exit(1);
     case 'v':
       Verbose = 1;
@@ -69,19 +72,20 @@ int main(int argc, char **argv) {
     fprintf(stderr, "dump_2iso: error: fail to open '%s'\n", input_path);
     exit(1);
   }
-  FREAD(&header, sizeof header, 1);
-  fprintf(stderr, "verbose: %d\n", header.version);
-  fprintf(stderr, "t: %g\n", header.t);
-  fprintf(stderr, "len: %ld\n", header.len);
-  fprintf(stderr, "npe: %d\n", header.npe);
-  fprintf(stderr, "depth: %d\n", header.depth);
-  fprintf(stderr, "i: %d\n", header.i);
-  fprintf(stderr, "n: [%g %g %g]\n", header.n.x, header.n.y, header.n.z);
-  if ((names = malloc(header.len * sizeof *names)) == NULL) {
+  FREAD(&context.header, sizeof context.header, 1);
+  fprintf(stderr, "verbose: %d\n", context.header.version);
+  fprintf(stderr, "t: %g\n", context.header.t);
+  fprintf(stderr, "len: %ld\n", context.header.len);
+  fprintf(stderr, "npe: %d\n", context.header.npe);
+  fprintf(stderr, "depth: %d\n", context.header.depth);
+  fprintf(stderr, "i: %d\n", context.header.i);
+  fprintf(stderr, "n: [%g %g %g]\n", context.header.n.x, context.header.n.y,
+          context.header.n.z);
+  if ((names = malloc(context.header.len * sizeof *names)) == NULL) {
     fprintf(stderr, "dump_2iso: error: malloc failed\n");
     exit(1);
   }
-  for (i = 0; i < header.len; i++) {
+  for (i = 0; i < context.header.len; i++) {
     FREAD(&len, sizeof len, 1);
     names[i] = malloc((len + 1) * sizeof *names[i]);
     FREAD(names[i], sizeof *names[i], len);
@@ -90,22 +94,23 @@ int main(int argc, char **argv) {
   }
   FREAD(o, sizeof o, 1);
   fprintf(stderr, "origin: [%g %g %g]\n", o[0], o[1], o[2]);
-  X0 = o[0];
-  Y0 = o[1];
-  Z0 = o[2];
-  L0 = o[3];
+  context.X0 = o[0];
+  context.Y0 = o[1];
+  context.Z0 = o[2];
+  context.L0 = o[3];
   fprintf(stderr, "size: %g\n", o[3]);
-  malloc_level = 0;
-  index = NULL;
-  if ((values = malloc(header.len * sizeof *values)) == NULL) {
+  context.malloc_level = 0;
+  context.index = NULL;
+  if ((context.values = malloc(context.header.len * sizeof *context.values)) ==
+      NULL) {
     fprintf(stderr, "dump_2iso: error: malloc failed\n");
     exit(1);
   }
-  nleaf = 0;
-  traverse(0);
-  fprintf(stderr, "nleaf: %ld\n", nleaf);
-  free(index);
-  for (i = 0; i < header.len; i++)
+  context.nleaf = 0;
+  traverse(0, process, NULL);
+  fprintf(stderr, "nleaf: %ld\n", context.nleaf);
+  free(context.index);
+  for (i = 0; i < context.header.len; i++)
     free(names[i]);
   free(names);
   if (fclose(input_file) != 0) {
@@ -114,47 +119,49 @@ int main(int argc, char **argv) {
   }
 }
 
-static void process(int level) {
-  int i, j;
+static void process(int level, void *context_v) {
+  int i;
   double Delta, x, y, z;
-  float xyz[8 * 3];
+  struct Context *context;
+  context = context_v;
+
   x = 0;
   y = 0;
   z = 0;
   for (i = 1; i <= level; i++) {
-    Delta = L0 * (1. / (1 << i));
-    x += Delta * (shift[index[i]][0] - 0.5);
-    y += Delta * (shift[index[i]][1] - 0.5);
-    z += Delta * (shift[index[i]][2] - 0.5);
+    Delta = context->L0 * (1. / (1 << i));
+    x += Delta * shift[context->index[i]][0];
+    y += Delta * shift[context->index[i]][1];
+    z += Delta * shift[context->index[i]][2];
   }
-  j = 0;
-  for (i = 0; i < 8; i++) {
-    xyz[j++] = x + Delta * (shift[i][0] - 0.5);
-    xyz[j++] = y + Delta * (shift[i][1] - 0.5);
-    xyz[j++] = z + Delta * (shift[i][2] - 0.5);
-  }
-  nleaf++;
+  fprintf(stderr, "x, y, z: %g %g %g\n", x, y, z);
+  context->nleaf++;
 }
 
-static long traverse(int level) {
+static long traverse(int level, void (*process)(int, void *), void *context_v) {
   enum { leaf = 2 };
   unsigned flags;
   long size, size0;
+  struct Context *context;
+  context = context_v;
+
   FREAD(&flags, sizeof flags, 1);
-  FREAD(values, sizeof *values, header.len);
-  size = values[0];
+  FREAD(context->values, sizeof *context->values, context->header.len);
+  size = context->values[0];
   size0 = 1;
   if (flags & leaf)
-    process(level);
+    process(level, context);
   if (flags & leaf) {
     /* */
   } else {
-    while (level + 1 >= malloc_level) {
-      malloc_level = 2 * malloc_level + 2;
-      index = realloc(index, malloc_level * sizeof *index);
+    while (level + 1 >= context->malloc_level) {
+      context->malloc_level = 2 * context->malloc_level + 2;
+      context->index = realloc(context->index,
+                               context->malloc_level * sizeof *context->index);
     }
-    for (index[level + 1] = 0; index[level + 1] < 8; index[level + 1]++)
-      size0 += traverse(level + 1);
+    for (context->index[level + 1] = 0; context->index[level + 1] < 8;
+         context->index[level + 1]++)
+      size0 += traverse(level + 1, process, context);
   }
   assert(size0 == size);
   return size;
