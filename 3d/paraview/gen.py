c3350aae257125b10587695bb8f2f2068a02a144
Sergey Litvinov <slitvinov@gmail.com>
2024-02-09 12:28:26 -0500

    update

--- 3d/paraview/gen.py
+++ 3d/paraview/gen.py
@@ -1,9 +1,18 @@
 import collections
 import struct
 import os
+import sys
 
+shift = ((0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1),
+         (1, 1, 0), (1, 1, 1))
 
-def indicator(x, y, z):
+def ellipse(x, y, z):
+    xc = 1 / 2
+    yc = 1 / 2
+    zc = 1 / 2
+    rx = 1 / 4
+    ry = 1 / 5
+    rz = 1 / 6
     return (x - xc)**2 / rx**2 + (y - yc)**2 / ry**2 + (z - zc)**2 / rz**2 - 1
 
 
@@ -53,18 +62,8 @@ def traverse(level):
     dump.seek(curr, os.SEEK_SET)
     return cell_size
 
-
-shift = ((0, 0, 0), (0, 0, 1), (0, 1, 0), (0, 1, 1), (1, 0, 0), (1, 0, 1),
-         (1, 1, 0), (1, 1, 1))
 minlevel = 1
 maxlevel = 6
-xc = 1 / 2
-yc = 1 / 2
-zc = 1 / 2
-rx = 1 / 4
-ry = 1 / 5
-rz = 1 / 6
-cs = 0
 fields = "size", "cs"
 nfields = len(fields)
 t = 0
@@ -76,10 +75,11 @@ n = 0, 0, 0
 X0, Y0, Z0 = 0, 0, 0
 L0 = 1
 index = collections.defaultdict(int)
+indicator = ellipse
 with open("gen.dump", "wb") as dump:
     dump.write(struct.pack("dl4i3d", t, nfields, i, depth, npe, version, *n))
     for field in fields:
         fmt = "I%ds" % len(field)
         dump.write(struct.pack(fmt, len(field), str.encode(field)))
     dump.write(struct.pack("4d", X0, Y0, Z0, L0))
-    print("cells: ", traverse(0))
+    sys.stderr.write("cells: %ld\n" % traverse(0))
