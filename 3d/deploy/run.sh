b0921e701e5ce2f8d8fec1e27a9f2b7e7fb79cac
Sergey Litvinov <slitvinov@gmail.com>
2024-02-28 17:42:17 -0500

    up

--- 3d/deploy/run.sh
+++ 3d/deploy/run.sh
@@ -2,5 +2,5 @@
 
 # ../stl/cylinder.py
 ../stl/center.py $HOME/cfd.stl
- ../paraview/stl2dump -v -- -4.8 -6 -6 12    7 9   64 center.stl basilisk.dump
-exec mpiexec ./cylinder -v -r 220 -l 7 -m 9 -p 10 -e 3000 -d basilisk.dump -o h
+../paraview/stl2dump -v -- -4.8 -6 -6 12    7 10   64 center.stl basilisk.dump
+exec mpiexec ./cylinder -v -r 220 -l 7 -m 10 -p 10 -e 3000 -d basilisk.dump -o h
