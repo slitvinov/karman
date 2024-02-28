d18943ba90f3de70af11a6ecdf7dc92c351299ee
Sergey Litvinov <slitvinov@gmail.com>
2024-02-28 17:20:20 -0500

    cfd.stl

--- 3d/deploy/run.sh
+++ 3d/deploy/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # ../stl/cylinder.py
-../stl/center.py $HOME/'No helix.stl'
+../stl/center.py $HOME/cfd.stl
  ../paraview/stl2dump -v -- -4.8 -6 -6 12    7 9   64 center.stl basilisk.dump
 exec mpiexec ./cylinder -v -r 220 -l 7 -m 9 -p 10 -e 3000 -d basilisk.dump -o h
