f97493beca7f03225a8c8d7b3ee0abe363e744fe
Sergey Litvinov <slitvinov@gmail.com>
2024-03-21 13:26:05 -0400

    up

--- 3d/deploy/cylinder.c
+++ 3d/deploy/cylinder.c
@@ -20602,9 +20602,9 @@ static int dump_0_expr0(int *ip,double *tp,Event *_ev){int i=*ip;double t=*tp;in
   if (AdaptFlag && i > 2) {
     astats s = adapt_wavelet((struct Adapt){(scalar *)((scalar[]){cs, u.x, u.y, u.z,{-1}}), (double[]){0, 0.01, 0.01, 0.01},
                              .maxlevel = maxlevel, .minlevel = minlevel});
-    fractions_cleanup((struct Cleanup){cs, fs});
+
     do { static const int too_fine = 1 << user; {foreach_cell() { if (is_leaf(cell)) continue; if (is_local(cell) && (!(x < X0 + 0.9 * L0) && level > outlevel)) cell.flags |= too_fine; }end_foreach_cell();} for (int _l = depth(); _l >= 0; _l--) { {foreach_cell() { if (is_leaf(cell)) continue; if (level == _l) { if (is_local(cell) && (cell.flags & too_fine)) { coarsen_cell (point, all); cell.flags &= ~too_fine; } continue; } }end_foreach_cell();} mpi_boundary_coarsen (_l, too_fine); } mpi_boundary_update (all); } while (0);
-    fractions_cleanup((struct Cleanup){cs, fs});
+
     if (Verbose && i % period == 0 && pid() == 0)
       fprintf(ferr, "cylinder: refined %d cells, coarsened %d cells\n", s.nf,
               s.nc);
